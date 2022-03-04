6266d47b4fc7c8386d9d7f6b905981b01efe6633
Your Name <you@example.com>
2022-03-04 12:41:40 +0200

    second commit

--- skript.js
+++ skript.js
@@ -1,18 +1,36 @@
-const numberOfFilms = +prompt('How many films your watch?', '');
+let numberOfFilms;
+do {
+    numberOfFilms = prompt('How many films your watch?', '');
+} while (numberOfFilms.length == 0);
+
+
 
 const personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
- const quest1 = prompt('one of the last waching film?', ''),
-       quest2 = prompt('How many estimate thim?', ''),
-       quest3 = prompt('one of the last waching film?', ''),
-       quest4 = prompt('How many estimate thim?', '');
-       personalMovieDB.movies[quest1] = quest2;
-       personalMovieDB.movies[quest3] = quest4;
-       console.log(personalMovieDB);
-    const a = 8;
-    console.log(a);
+
+let quest1,
+    quest2;
+for (let i = 0; i < 2; i++) {
+
+    do {
+        quest1 = prompt('one of the last waching film?', '');
+    } while (quest1.length == 0 || quest1.length > 50);
+    quest2 = prompt('How many estimate thim?', '');
+    personalMovieDB.movies[quest1] = quest2;
+    if (quest1.length == 0 || quest2.length > 50 || quest2.length == 0) {
+        i--;
+    }
+}
+console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    alert('ВИ класический зритель!!');
+} else {
+    alert('ДА ви кіноман!!!!');
+}
